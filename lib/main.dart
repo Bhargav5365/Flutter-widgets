e7f4c29afb09865bafbbdc52dd79e49d587fa4f3
Bhargav <bhargav.bhavani@odaninfotech.com>
2022-06-13 17:54:08 +0530

    AnimatedContainer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/practice/AnimatedBuilder.dart';
+import 'package:flutter_widgets/practice/AnimatedContainer.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
+        body: Animate(),
       ),
     );
   }

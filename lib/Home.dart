aa00f08a7996081588631f638e2c45e1c7f52a5f
Bhargav <bhargav.bhavani@odaninfotech.com>
2022-06-16 17:14:13 +0530

    Form

--- lib/Home.dart
+++ lib/Home.dart
@@ -34,6 +34,7 @@ import 'package:flutter_widgets/practice/CupertinoTextField.dart';
 import 'package:flutter_widgets/practice/CupertinoTimerPicker.dart';
 import 'package:flutter_widgets/practice/DecoratedBoxTransition.dart';
 import 'package:flutter_widgets/practice/ElevatedButton.dart';
+import 'package:flutter_widgets/practice/Form.dart';
 import 'package:flutter_widgets/practice/FutureBuilder.dart';
 import 'package:flutter_widgets/practice/Image%20Icon.dart';
 import 'package:flutter_widgets/practice/MergeSemantics.dart';
@@ -1337,7 +1338,34 @@ class _HomeState extends State<Home> {
                         ),
                       ),
                     ),
-                    Spacer(),
+                    Spacer(),                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                DDD(),
+                          ),
+                        );
+                        print('daf');
+                      },
+                      child: Container(
+                        height: screenSize.height * 0.1,
+                        width: screenSize.width * 0.3,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[400],
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        child: Center(
+                          child: Text('Form',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                    ),
+
                   ],
                 ),
               ],
---
+++ lib/practice/Form.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class DDD extends StatefulWidget {
+  const DDD({Key? key}) : super(key: key);
+
+  @override
+  State<DDD> createState() => _DDDState();
+}
+
+class _DDDState extends State<DDD> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your email',
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Process data.
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

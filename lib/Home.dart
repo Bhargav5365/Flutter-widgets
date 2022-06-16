31c0a32c0704a0ac3e01c097f1daab78e42d010b
Bhargav <bhargav.bhavani@odaninfotech.com>
2022-06-16 11:08:51 +0530

    CupertinoPicker

--- lib/Home.dart
+++ lib/Home.dart
@@ -20,6 +20,7 @@ import 'package:flutter_widgets/practice/CupertinoContextMenu.dart';
 import 'package:flutter_widgets/practice/CupertinoDatePicker.dart';
 import 'package:flutter_widgets/practice/CupertinoNavigationBar.dart';
 import 'package:flutter_widgets/practice/CupertinoPageScaffold.dart';
+import 'package:flutter_widgets/practice/CupertinoPicker.dart';
 import 'package:flutter_widgets/practice/DecoratedBoxTransition.dart';
 import 'package:flutter_widgets/practice/ElevatedButton.dart';
 import 'package:flutter_widgets/practice/FutureBuilder.dart';
@@ -969,6 +970,32 @@ class _HomeState extends State<Home> {
                       ),
                     ),
                     Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CupertinoPickerSample(),
+                          ),
+                        );
+                        print('daf');
+                      },
+                      child: Container(
+                        height: screenSize.height * 0.1,
+                        width: screenSize.width * 0.3,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[400],
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        child: Center(
+                          child: Text('CupertinoPicker',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ],
---
+++ lib/practice/CupertinoPicker.dart
@@ -0,0 +1,92 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+const double _kItemExtent = 32.0;
+const List<String> _fruitNames = <String>[
+  'Apple',
+  'Mango',
+  'Banana',
+  'Orange',
+  'Pineapple',
+  'Strawberry',
+];
+
+class CupertinoPickerSample extends StatefulWidget {
+  const CupertinoPickerSample({Key? key}) : super(key: key);
+
+  @override
+  State<CupertinoPickerSample> createState() => _CupertinoPickerSampleState();
+}
+
+class _CupertinoPickerSampleState extends State<CupertinoPickerSample> {
+  int _selectedFruit = 0;
+
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => Container(
+        height: 216,
+        padding: const EdgeInsets.only(top: 6.0),
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        color: CupertinoColors.systemBackground.resolveFrom(context),
+        child: SafeArea(
+          top: false,
+          child: child,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: DefaultTextStyle(
+        style: TextStyle(
+          color: CupertinoColors.label.resolveFrom(context),
+          fontSize: 22.0,
+        ),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Selected fruit: '),
+              CupertinoButton(
+                padding: EdgeInsets.zero,
+                onPressed: () => _showDialog(
+                  CupertinoPicker(
+                    magnification: 1.22,
+                    squeeze: 1.2,
+                    useMagnifier: true,
+                    itemExtent: _kItemExtent,
+                    onSelectedItemChanged: (int selectedItem) {
+                      setState(
+                        () {
+                          _selectedFruit = selectedItem;
+                        },
+                      );
+                    },
+                    children:
+                        List<Widget>.generate(_fruitNames.length, (int index) {
+                      return Center(
+                        child: Text(
+                          _fruitNames[index],
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+                child: Text(
+                  _fruitNames[_selectedFruit],
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

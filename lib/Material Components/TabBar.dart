07467ff0a8dd13dac995c00fc56006a7b54f0075
Bhargav <bhargav.bhavani@odaninfotech.com>
2022-06-17 13:25:53 +0530

    TabBar

---
+++ lib/Material Components/TabBar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class XXX extends StatelessWidget {
+  const XXX({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'TabBar Widget',
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+          ),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.cloud_outlined),
+              ),
+              Tab(
+                icon: Icon(Icons.beach_access_sharp),
+              ),
+              Tab(
+                icon: Icon(Icons.brightness_5_sharp),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text("It's cloudy here"),
+            ),
+            Center(
+              child: Text("It's rainy here"),
+            ),
+            Center(
+              child: Text("It's sunny here"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
